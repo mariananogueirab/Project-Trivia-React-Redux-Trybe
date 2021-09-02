04791d07c160ed3e664125192ca7f50352f14673
Rodrigo Cesar Destro Fidencio <60055837+rodrigocdfidencio@users.noreply.github.com>
2021-09-02 17:31:51 -0300

    Resolvendo conflitos do git igonre 2

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import logo from './trivia.png';
 import './App.css';
 import Login from './pages/Login';
-<<<<<<< HEAD
 import Settings from './pages/Settings';
-=======
->>>>>>> ae0837a9b8a2043f5b87b3ea50005a378835b2a5
-
 export default class App extends React.Component {
   render() {
     return (
@@ -17,10 +13,7 @@ export default class App extends React.Component {
         </header>
         <Switch>
           <Route exact path="/" component={ Login } />
-<<<<<<< HEAD
           <Route exact path="/settings" component={ Settings } />
-=======
->>>>>>> ae0837a9b8a2043f5b87b3ea50005a378835b2a5
         </Switch>
       </div>
     );
