9d5e014908a87e2fdc823bdd6df01279a1fac9b1
Rodrigo Cesar Destro Fidencio <60055837+rodrigocdfidencio@users.noreply.github.com>
2021-09-08 18:34:00 -0300

    [GRUPO 1] - Requisito 14

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 
 class Feedback extends React.Component {
   render() {
-    const { player: { assertions } } = JSON.parse(localStorage.getItem('state'));
+    const { player: { assertions, score } } = JSON.parse(localStorage.getItem('state'));
     const minAssertions = 3;
     return (
       <div>
         <h1 data-testid="feedback-text">
           { assertions < minAssertions ? 'Podia ser melhor...' : 'Mandou bem!'}
         </h1>
+        <p>
+          O total de respostas certas foi:
+          <span data-testid="feedback-total-question">{ assertions }</span>
+        </p>
+        <p>
+          Seu score final é:
+          <span data-testid="feedback-total-score">{ score }</span>
+        </p>
       </div>
     );
   }
